e6ce8ff702923fdf5fbcda054ebc630d54b5ab17
emilio.calistok9@gmail.com <emilio.calistok9@gmail.com>
2020-06-24 04:33:57 -0400

    docs agregados

---
+++ docs/precache-manifest.85bbc2c712e8746f89c92f7a85cfe124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21033c40f621291c14ffb5762f33193a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "245d69998300d3303757",
+    "url": "/static/css/main.fe68a24f.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245d69998300d3303757",
+    "url": "/static/js/main.dc85ac1a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
